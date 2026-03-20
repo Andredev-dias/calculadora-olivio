058854bd83dfc1797e65414821a8369aefa865db
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2026-03-20 11:18:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function somar(){
+function soma(){
     var n1 = document.getElementById('n1').valueAsNumber
     var n2 = document.getElementById('n2').valueAsNumber
-    var resp = document.getElementById('resposta')
-    resp.textContent = n1 + n2
+    var resposta = document.getElementById('respSoma') 
+    resposta.textContent = n1 + n2
+}
+
+function sub(){
+    var n1 = document.getElementById('n3').valueAsNumber
+    var n2 = document.getElementById('n4').valueAsNumber
+    var resposta = document.getElementById('respSub') 
+    resposta.textContent = n1 - n2
+}
+
+
+function mult(){
+    var n1 = document.getElementById('n5').valueAsNumber
+    var n2 = document.getElementById('n6').valueAsNumber
+    var resposta = document.getElementById('respMult') 
+    resposta.textContent = n1*- n2
+}
+
+
+function sub(){
+    var n1 = document.getElementById('n7').valueAsNumber
+    var n2 = document.getElementById('n8').valueAsNumber
+    var resposta = document.getElementById('respDiv') 
+    resposta.textContent = n1 / n2
 }
